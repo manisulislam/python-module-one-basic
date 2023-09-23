b974ed4a521ebb52463218130447f13c826176cf
anisul islam <anisulislam980340@gmail.com>
2023-09-23 23:35:54 +0600

    variable , comment , fstring

---
+++ variable.py
@@ -0,0 +1,19 @@
+age=22
+interest_rate= 2.5
+is_single= True 
+address='sultanpur'
+# multiline comment below
+
+"""  """
+print(age)
+print(address)
+
+print(type(interest_rate))
+print(type(age))
+
+# single line comment below
+# print(type(is_single))
+
+print("kodom ali"+" "+ "kacha badam")
+text= f"kodomer bari {address} ter age is {age} and interest rate is {interest_rate}"
+print(text)
