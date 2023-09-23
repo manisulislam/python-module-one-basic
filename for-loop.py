b454d5d7712c8602f192ed0c1ac56e1b100914b8
anisul islam <anisulislam980340@gmail.com>
2023-09-24 03:16:50 +0600

    for loop and range

---
+++ for-loop.py
@@ -0,0 +1,16 @@
+numbers = [5,10,15,20,25]
+sum=0
+for num in numbers:
+    # print(num)
+    sum= sum+ num
+print(sum)
+
+
+# range dea korte chile
+for i in range(1,10,2):
+    print(i)
+
+
+friends= ['anis', 'ahad', 'forhad']
+for friend in friends:
+    print(friend)
