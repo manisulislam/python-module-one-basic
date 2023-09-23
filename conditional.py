7f0b380a1248c2358288ec77184885b943002462
anisul islam <anisulislam980340@gmail.com>
2023-09-24 02:56:36 +0600

    while loop solved

---
+++ conditional.py
@@ -0,0 +1,22 @@
+a=5
+boss = False
+if(a>6):
+    print("2 er besi")
+elif a>3:
+    print("olpo boro")
+elif a==2:
+    print("doitai soman")
+else:
+    print("kicoi hoi ni")
+
+
+if boss is True:
+    print("kaj koro")
+else:
+    print("kaj korbe na")
+
+
+if boss is not True:
+    print("beton pabe na")
+else:
+    print("beton pabe")
