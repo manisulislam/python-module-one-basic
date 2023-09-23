242d7ea4ed3e2fa752287edccf087434e3a2baa1
anisul islam <anisulislam980340@gmail.com>
2023-09-24 00:04:04 +0600

    type casting, and input

---
+++ input.py
@@ -0,0 +1,11 @@
+first_money= input("give me money: ")
+second_money= input("give me second money: ")
+first_money_int= int(first_money)
+second_money_int= int(second_money)
+print(type(first_money_int))
+total=first_money+ second_money
+total_1 = first_money_int+ second_money_int
+
+print(total)
+print(total_1)
+
